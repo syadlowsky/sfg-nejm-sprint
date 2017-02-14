9a564ff041b9fba08f135a17f8722f023e9b45f3
Steve Yadlowsky <syadlows@stanford.edu>
2017-02-14 15:34:35 -0800

    Switch to SAEs

--- sites_analysis.R
+++ sites_analysis.R
@@ -12,9 +12,9 @@ safety <- read.csv("data/safety.csv")
 outcomes <- read.csv("data/outcomes.csv")
 sites <- read.csv("sites/networkXid.csv")
 
-merged.data <- merge.data.frame(baseline, safety, by="MASKID")
-merged.data <- merge.data.frame(merged.data, outcomes, by="MASKID")
-merged.data <- merge.data.frame(merged.data, sites, by="NEWSITEID", all.x = T)
+merged.data <- merge(baseline, safety, by="MASKID")
+merged.data <- merge(merged.data, outcomes, by="MASKID")
+merged.data <- merge(merged.data, sites, by="NEWSITEID", all.x = T)
 
 data.with.site <- merged.data
 net.levels <- levels(data.with.site$NET)
@@ -27,11 +27,21 @@ survival.outcome <- with(data.with.site, Surv(time=SAE_DAYS, event=SAE_EVNT))
 cox.fit <- with(data.with.site, coxph(survival.outcome ~ INTENSIVE * NET))
 print(summary(cox.fit))
 
-sae.events.all.patients <- merged.data[, c("REL_SAE_EVNT", "HYP_SAE_EVNT", "SYN_SAE_EVNT", "BRA_SAE_EVNT",
-                                               "ELE_SAE_EVNT", "INJ_SAE_EVNT", "AKI_SAE_EVNT", "HYP_ERS_EVNT", "SYN_ERS_EVNT",
-                                               "BRA_ERS_EVNT", "ELE_ERS_EVNT", "INJ_ERS_EVNT", "AKI_ERS_EVNT", "LON_MCE_EVNT",
-                                               "HIN_MCE_EVNT", "LOK_MCE_EVNT", "HIK_MCE_EVNT", "ALO_OHO_EVNT", "WDZ_OHO_EVNT")]
+treated.arm <- data.with.site[data.with.site$INTENSIVE==1,]
+control.arm <- data.with.site[data.with.site$INTENSIVE==0,]
+rate <- with(treated.arm, tapply(SAE_EVNT, NET, mean))
+rate <- rate - with(control.arm, tapply(SAE_EVNT & !INTENSIVE, NET, mean))
 
-southeast <- !is.na(merged.data$NET) & merged.data$NET=="SOUTHEAST"
-southeast.patients <- sae.events.all.patients[southeast, ]
-(sum(sae.events.all.patients)/sum(southeast.patients)) * colSums(southeast.patients) / colSums(sae.events.all.patients)
+se <- with(treated.arm, tapply(SAE_EVNT, NET, sd) / sqrt(tapply(SAE_EVNT, NET, NROW)))
+se <- sqrt(se^2 + with(control.arm, tapply(SAE_EVNT, NET, sd) / sqrt(tapply(SAE_EVNT, NET, NROW)))^2)
+
+network <- rownames(se)
+setEPS()
+postscript("sae_network_interaction.eps", height=6, width=8)
+require(plotrix)
+par(mar=c(5,5,3,3))
+plotCI(1:5, rate, ui=rate+1.28*se, li=rate-1.28*se, ylim=c(-0.1, 0.1), xlab="Network", ylab="Change in SAE incidence with treatment", main="Network vs effect of treatment on SAE",
+       axes=F, mgp=c(3,0,0))
+axis(2)
+axis(1, at=1:5, labels=as.character(network), las=1)
+dev.off()
--- subgroup_analysis.R
+++ subgroup_analysis.R
@@ -1,3 +1,7 @@
+library(survival)
+library(causalTree)
+library(dplyr)
+
 setwd("~/phd/sfg/nejm-sprint")
 baseline <- read.csv("data/baseline.csv")
 safety <- read.csv("data/safety.csv")
@@ -5,29 +9,29 @@ outcomes <- read.csv("data/outcomes.csv")
 merged.data <- merge.data.frame(baseline, safety, by="MASKID")
 merged.data <- merge.data.frame(merged.data, outcomes, by="MASKID")
 
-outcome <- merged.data$EVENT_PRIMARY & merged.data$T_PRIMARY <= 365.25*5
-interaction.model <- coxph(Surv(merged.data$T_PRIMARY, outcome) ~ INTENSIVE*RACE4 + INTENSIVE*FEMALE + INTENSIVE*BMI + INTENSIVE*ASPIRIN + INTENSIVE*SMOKE_3CAT + INTENSIVE*STATIN,
+outcome <- Surv(merged.data$SAE_DAYS, merged.data$SAE_EVNT)
+interaction.model <- coxph(outcome ~ INTENSIVE*RACE4 + INTENSIVE*FEMALE + INTENSIVE*BMI + INTENSIVE*ASPIRIN + INTENSIVE*SMOKE_3CAT + INTENSIVE*STATIN,
                            data=merged.data)
 
-
-sae.interaction.model <- coxph(Surv(merged.data$SAE_DAYS, merged.data$SAE_EVNT) ~ INTENSIVE*RACE4 + INTENSIVE*FEMALE + INTENSIVE*BMI + INTENSIVE*ASPIRIN + INTENSIVE*SMOKE_3CAT + INTENSIVE*STATIN,
-                           data=merged.data)
-
-library(causalTree)
-tree <- causalTree(outcome ~ RACE4 + FEMALE + BMI + ASPIRIN + SMOKE_3CAT + STATIN, data = merged.data, treatment = merged.data$INTENSIVE,
+tree <- causalTree(SAE_EVNT ~ RACE4 + FEMALE + BMI + ASPIRIN + SMOKE_3CAT + STATIN, data = merged.data, treatment = merged.data$INTENSIVE,
                    split.Rule = "CT", cv.option = "CT", split.Honest = T, cv.Honest = T, split.Bucket = F, 
                    xval = 5, cp = 0, minsize = 50, propensity = 0.5)
 opcp <- tree$cptable[,1][which.min(tree$cptable[,4])]
 opfit <- prune(tree, opcp)
 svg("honest_tree_pruned.svg", height=6, width=8)
 rpart.plot(opfit)
-
+dev.off()
 # See how this relationship looks
 merged.data$bmi.quantile <- ntile(merged.data$BMI, 10)
-rate <- tapply(outcome & merged.data$INTENSIVE, merged.data$bmi.quantile, mean)
-rate <- rate - tapply(outcome & !merged.data$INTENSIVE, merged.data$bmi.quantile, mean)
+rate <- tapply(merged.data$SAE_EVNT & merged.data$INTENSIVE, merged.data$bmi.quantile, mean)
+rate <- rate - tapply(merged.data$SAE_EVNT & !merged.data$INTENSIVE, merged.data$bmi.quantile, mean)
+
+se <- tapply(merged.data$SAE_EVNT & merged.data$INTENSIVE, merged.data$bmi.quantile, sd)
+se <- (se + tapply(merged.data$SAE_EVNT & !merged.data$INTENSIVE, merged.data$bmi.quantile, sd))/sqrt(tapply(merged.data$SAE_EVNT & !merged.data$INTENSIVE, merged.data$bmi.quantile, NROW))
+
 bmi <- tapply(merged.data$BMI, merged.data$bmi.quantile, mean)
 setEPS()
-postscript("ate_bmi_interaction.eps", height=6, width=8)
-plot(bmi, rate, xlab="BMI", ylab="Average effect of intensive treatment", main="BMI vs effect of treatment on primary outcome")
-
+postscript("sae_bmi_interaction.eps", height=6, width=8)
+require(plotrix)
+plotCI(bmi, rate, ui=rate+1.96*se, li=rate-1.96*se, xlab="BMI", ylab="Change in SAE incidence with treatment", main="BMI vs effect of treatment on primary outcome")
+dev.off()
